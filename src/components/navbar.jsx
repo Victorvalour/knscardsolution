7c3be176cc52edea39c570c87b98ccb0ee3a93fe
Victorvalour <ewehvictor7@gmail.com>
2023-09-25 14:32:57 +0100

    update

---
+++ src/components/navbar.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { motion, useCycle, AnimatePresence } from "framer-motion";
+import Logo from "../images/KNS-LOGO-PNG.png"
+
+export default function Navbar() {
+
+    const [mobileNav, toggleMobileNav] = useCycle(false, true)
+    
+    return (
+
+<section className="flex drop-shadow-lg bg-white justify-between w-full h-20 pt-3">
+<div className="flex items-center">
+    <img src={Logo} alt="" className="w-20 h-16 p-0" />
+    <h2 className="font-gothic"><span className="text-cobalt text-lg">KNS</span><span className="text-orange-500">CARD</span>SOLUTION</h2>
+</div> 
+<div>
+        <motion.button 
+        animate={mobileNav ? "open" : "closed"}
+        onClick= {() => toggleMobileNav()}
+        className="flex flex-col space-y-1 mt-5 mr-3"
+        >
+            <motion.span variants={{
+                closed: { rotate:0, y: 0},
+                open: { rotate:45, y:10},
+            }} 
+            className="w-12 h-1.5 bg-orange-500 block rounded-sm"></motion.span>
+            <motion.span variants={{
+                closed: { opacity: 1},
+                open: { opacity: 0},
+            }} 
+            className="w-12 h-1.5 bg-orange-500 block rounded-sm"></motion.span>
+            <motion.span  variants={{
+                closed: { rotate: 0, y:0},
+                open: { rotate: -45, y:-10},
+            }} 
+             className="w-12 h-1.5 bg-orange-500 block rounded-sm"></motion.span>
+        </motion.button>
+</div>
+    <AnimatePresence>
+{mobileNav && ( <motion.div 
+key="mobile-nav"
+variants={{
+    open:{
+        x: "0%"
+    },
+    closed: {
+        x: "-180%"
+    }
+}} 
+initial="closed"
+animate="open"
+exit="closed"
+
+className="fixed right-0 top-20 shadow-[10px_10px_10px_0px_rgba(0,0,0,0.8)] bg-slate-500 w-64">
+            <div>
+                <ul className="flex flex-col text-center my-10 space-y-12">
+                    <li className="border-b-2">
+                        <a href="#"
+                        className="text-2xl font-bold text-white">Home</a>
+                    </li>
+                    <li className="border-b-2">
+                        <a href="#"
+                        className="text-2xl font-bold text-white ">About</a>
+                    </li>
+                    <li className="border-b-2">
+                        <a href="#"
+                        className="text-2xl font-bold text-white">Contact Us</a>
+                    </li>
+                    <li className="">
+                        <a href="#"
+                        className="text-2xl font-bold text-white">Create a free account</a>
+                    </li>
+                </ul>
+            </div>
+</motion.div>)}
+</AnimatePresence>
+</section>
+    )
+}
--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -1,88 +1,39 @@
 import React from "react";
 import { motion, useCycle, AnimatePresence } from "framer-motion";
-import Logo from "../images/KNS-LOGO-PNG.png"
+import Navbar from "../components/navbar";
+import creditCard from "../images/credit-card.png"
 
 
 export default function Landing() {
 
-const [mobileNav, toggleMobileNav] = useCycle(false, true)
 
 return (
     <div className="flex flex-col">
-        <section className="flex drop-shadow-lg bg-white justify-between w-full h-20 pt-3">
-            <div className="flex items-center">
-                <img src={Logo} alt="" className="w-20 h-16 p-0" />
-                <h2 className="font-gothic"><span className="text-cobalt text-lg">KNS</span><span className="text-orange-500">CARD</span>SOLUTION</h2>
-            </div> 
-            <div>
-                    <motion.button 
-                    animate={mobileNav ? "open" : "closed"}
-                    onClick= {() => toggleMobileNav()}
-                    className="flex flex-col space-y-1 mt-5"
-                    >
-                        <motion.span variants={{
-                            closed: { rotate:0, y: 0},
-                            open: { rotate:45, y:10},
-                        }} 
-                        className="w-12 h-1.5 bg-orange-500 block rounded-sm"></motion.span>
-                        <motion.span variants={{
-                            closed: { opacity: 1},
-                            open: { opacity: 0},
-                        }} 
-                        className="w-12 h-1.5 bg-orange-500 block rounded-sm"></motion.span>
-                        <motion.span  variants={{
-                            closed: { rotate: 0, y:0},
-                            open: { rotate: -45, y:-10},
-                        }} 
-                         className="w-12 h-1.5 bg-orange-500 block rounded-sm"></motion.span>
-                    </motion.button>
-            </div>
-                <AnimatePresence>
-            {mobileNav && ( <motion.div 
-            key="mobile-nav"
-            variants={{
-                open:{
-                    x: "0%"
-                },
-                closed: {
-                    x: "-180%"
-                }
-            }} 
-            initial="closed"
-            animate="open"
-            exit="closed"
-        
-        className="fixed right-0 top-20 shadow-[10px_10px_10px_0px_rgba(0,0,0,0.8)] bg-slate-500 w-64">
-                        <div>
-                            <ul className="flex flex-col text-center my-10 space-y-12">
-                                <li>
-                                    <a href="#"
-                                    className="text-2xl font-bold text-white">Home</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                    className="text-2xl font-bold text-white ">Create free account</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                    className="text-2xl font-bold text-white">About</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                    className="text-2xl font-bold text-white">Contact Us</a>
-                                </li>
-                            </ul>
-                        </div>
-            </motion.div>)}
-            </AnimatePresence>
-        </section>
+
+            <Navbar />
+<div className="flex mb-5 mt-12">
               <div className="w-2/4 mb-5 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]">
-                <p className="text-4xl mb-5 mt-12 ml-5 font-bold">
+                <p className="text-4xl ml-5 font-bold">
 The <span className="text-cobalt">NO. 1</span> solution to all your card needs.
 </p>
+</div>
+<motion.div
+            id="card-spin"
+            className="w-2/4 h-full -z-10 p-4"
+            initial={{ rotate: 0 }}
+            animate={{ rotate: 360 }}
+            transition={{ duration: 20, repeat: Infinity, ease: "linear" }}
+          >
+            <img
+              src={creditCard}
+              alt="Credit Card"
+              className="w-full h-full"
+            />
+          </motion.div>
+      
         </div>
 
-        <button className="self-center rounded-md bg-gradient-to-r from-purple-500 to-cyan-600 hover:from-red-700 hover:to-purple-900 font-bold w-44 h-12 shadow-[0px_10px_10px_0px_rgba(25,53,171,0.75)]">Create a free account</button>
+        <button className="self-center rounded-md bg-gradient-to-r from-purple-800 to-cyan-800 hover:from-red-700 hover:to-purple-900 font-bold w-44 h-12 shadow-[0px_10px_10px_0px_rgba(25,53,171,0.75)] text-white">Create a free account</button>
 
         <div className="mt-8 mx-5">
             <p className="text-lg ">Have you been searching for ways on how to get virtual or physical cards?</p>
